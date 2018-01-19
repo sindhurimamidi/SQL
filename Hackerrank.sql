e2ce615f403c824ffa0a5b6a6790d9139a042909
Sindhuri Mamidi <sindhuri.mamidi30@gmail.com>
2018-01-19 10:58:27 -0800

    Create Hackerrank.sql

---
+++ Hackerrank.sql
@@ -0,0 +1,33 @@
+/*Advance Join Challenges*/
+
+/* You are given a table, Projects, containing three columns: Task_ID, Start_Date and End_Date. 
+It is guaranteed that the difference between the End_Date and the Start_Date is equal to 1 day for each row in the table.
+If the End_Date of the tasks are consecutive, then they are part of the same project. 
+Samantha is interested in finding the total number of different projects completed.
+Write a query to output the start and end dates of projects listed by the number of days it took to complete the project in ascending order. 
+If there is more than one project that have the same number of completion days, then order by the start date of the project. */
+
+SELECT Start_Date, End_Date
+FROM 
+    (SELECT Start_Date FROM Projects WHERE Start_Date NOT IN (SELECT End_Date FROM Projects)) a,
+    (SELECT End_Date FROM Projects WHERE End_Date NOT IN (SELECT Start_Date FROM Projects)) b 
+WHERE Start_Date < End_Date
+GROUP BY Start_Date 
+ORDER BY DATEDIFF(End_Date, Start_Date), Start_Date
+
+/* You are given three tables: Students, Friends and Packages. Students contains two columns: ID and Name. 
+Friends contains two columns: ID and Friend_ID (ID of the ONLY best friend). 
+Packages contains two columns: ID and Salary (offered salary in $ thousands per month).
+Write a query to output the names of those students whose best friends got offered a higher salary than them. 
+Names must be ordered by the salary amount offered to the best friends. 
+It is guaranteed that no two students got same salary offer.*/
+
+Select S.Name
+From (Students S join Friends F on S.ID = F.ID
+       join Packages P1 on S.ID=P1.ID
+       join Packages P2 on F.Friend_ID=P2.ID)
+Where P2.Salary > P1.Salary
+Order By P2.Salary;
+
+
+
