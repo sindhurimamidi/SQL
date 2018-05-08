84006b5adc57da5e45ec99f8d0daa459b7ec4ac2
Sindhuri Mamidi <sindhuri.mamidi30@gmail.com>
2018-05-08 14:08:14 -0700

    Update Hackerrank.sql
    Adding new queries.

--- Hackerrank.sql
+++ Hackerrank.sql
@@ -20,55 +20,91 @@ Friends contains two columns: ID and Friend_ID (ID of the ONLY best friend).
 Packages contains two columns: ID and Salary (offered salary in $ thousands per month).
 Write a query to output the names of those students whose best friends got offered a higher salary than them. 
 Names must be ordered by the salary amount offered to the best friends. 
-It is guaranteed that no two students got same salary offer.*/
-
+It is guaranteed that no two students got same salary offer.
+*/
 Select S.Name
 From (Students S join Friends F on S.ID = F.ID
-       join Packages P1 on S.ID=P1.ID
-       join Packages P2 on F.Friend_ID=P2.ID)
+      join Packages P1 on S.ID=P1.ID
+      join Packages P2 on F.Friend_ID=P2.ID)
 Where P2.Salary > P1.Salary
 Order By P2.Salary;
 
 /* Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name). 
 If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
-STATION: Field, type */
-
-	SELECT CITY, LENGTH(CITY) as mlen 
-	FROM STATION 
-	ORDER BY  mlen DESC
-	LIMIT 1;
+STATION: Field, type 
+*/
+SELECT CITY, LENGTH(CITY) as mlen 
+FROM STATION 
+ORDER BY  mlen DESC
+LIMIT 1;
 
 /* Query the Name of any student in STUDENTS who scored higher than  Marks. Order your output by the last three characters of each name. 
 If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
 STUDENTS  : ID, name , marks. */
 
-	select name
-	from students
-	where marks > 75
-	order by SUBSTRING(name, -3) asc, ID asc;
+select name
+from students
+where marks > 75
+order by SUBSTRING(name, -3) asc, ID asc;
 
 /* Write a query identifying the type of each record in the TRIANGLES table using its three side lengths. Output one of the following statements for each record in the table: TRIANGLE: A,B,C
-* Not A Triangle: The given values of A, B, and C don't form a triangle.
-* Equilateral: It's a triangle with  sides of equal length.
-* Isosceles: It's a triangle with  sides of equal length.
-* Scalene: It's a triangle with  sides of differing lengths. */
-	
-	select case 
+ * Not A Triangle: The given values of A, B, and C don't form a triangle.
+ * Equilateral: It's a triangle with  sides of equal length.
+ * Isosceles: It's a triangle with  sides of equal length.
+ * Scalene: It's a triangle with  sides of differing lengths. 
+ */	
+select case 
 	when a+b<=c or b+c<=a or a+c<=b then 'Not A Triangle' 
 	when a=b and b=c and c=a then 'Equilateral' 
 	when a=b or b=c or a=c then 'Isosceles'
 	else 'Scalene' end 
-	from TRIANGLES 
+from TRIANGLES 
 
 /* New Companies:
-select employee.company_code,company.founder,count(distinct employee.lead_manager_code),count(distinct employee.senior_manager_code),count(distinct employee.manager_code), count(distinct employee.employee_code) 
-from employee employee inner join company company on employee.company_code = company.company_code 
-group by employee.company_code,company.founder 
-order by employee.company_code;1. Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name). 
-If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
-	STATION: Field, type */
+   select employee.company_code,company.founder,count(distinct employee.lead_manager_code),count(distinct employee.senior_manager_code),count(distinct employee.manager_code), count(distinct employee.employee_code) 
+   from employee employee inner join company company on employee.company_code = company.company_code 
+   group by employee.company_code,company.founder 
+   order by employee.company_code;1. 
+   Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name). 
+   If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
+   STATION: Field, type 
+*/
+SELECT CITY, LENGTH(CITY) as mlen 
+FROM STATION 
+ORDER BY  mlen DESC
+LIMIT 1;
 
-	SELECT CITY, LENGTH(CITY) as mlen 
-	FROM STATION 
-	ORDER BY  mlen DESC
-	LIMIT 1;
+/* Movie and Genres:
+   For each genre in the data set, how many genres are the movies in that genre in, on average. 
+   For example, a Action movie is in 1.3 genres and the average Adventure movie is in 2.9 genres, 
+   so an Action movie is a better defined genre.
+Genres
+	id  name
+	1   Action
+	2   Adventure
+	3   Animation
+	4   Children's
+	5   Comedy
+Genres_movies 
+	id movie_id     genre_id
+	1  1(Toy Story) 3(Animation)
+	2  1(Toy Story) 4(Children's)
+	3  1(Toy Story) 5(Comedy)
+	4  2(GoldenEye) 1(Action)
+	5  2(GoldenEye) 2(Adventure)
+	6  2(GoldenEye) 16(Thriller)
+*/
+select g.name as genre_name,
+       avg(gm.count_genres) as avg_count
+from ((select movie_id, 
+	      count(*) as count_genres
+       from genres_movies 
+       group by movie_id) as gm
+       join 
+       (select id, 
+	       name
+	from genres) as g 
+        on g.id = gm.genre_id)
+group by g.name
+order by avg_count
+
