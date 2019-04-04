e640c54cf8fb562495bb606485a069869974ed45
Sindhuri Mamidi <sindhuri.mamidi30@gmail.com>
2019-04-04 16:03:47 -0700

    Update Hackerrank.sql
    Adding advanced sql select questions.

--- Hackerrank.sql
+++ Hackerrank.sql
@@ -1,5 +1,5 @@
 /*Advance Join Challenges*/
-
+---------------------------
 /* You are given a table, Projects, containing three columns: Task_ID, Start_Date and End_Date. 
 It is guaranteed that the difference between the End_Date and the Start_Date is equal to 1 day for each row in the table.
 If the End_Date of the tasks are consecutive, then they are part of the same project. 
@@ -219,3 +219,38 @@ having c_count = (select max(t.count_c)
                    group by t.cnt
                    having count(t.cnt) = 1)
 order by c_count desc, h.hacker_id;
+
+/* Advanced select */
+--------------------
+/* The PADS */	
+select concat(name,'(',left(occupation,1),')')
+from occupations
+order by name;
+
+select CONCAT('There are a total of ',count(occupation),' ',LOWER(occupation),'s.')
+from occupations
+group by occupation
+order by count(occupation) ASC, occupation ASC;	
+
+/* Pivot the Occupation column in OCCUPATIONS so that each Name is sorted alphabetically and displayed underneath its corresponding Occupation. The output column headers should be Doctor, Professor, Singer, and Actor, respectively.
+Note: Print NULL when there are no more names corresponding to an occupation. 
+Occupations : name, occupation */
+set @r1=0, @r2=0, @r3=0, @r4=0;
+select min(Doctor), min(Professor), min(Singer), min(Actor)
+from(
+  select 
+     case 
+       when Occupation='Doctor' then (@r1:=@r1+1)
+       when Occupation='Professor' then (@r2:=@r2+1)
+       when Occupation='Singer' then (@r3:=@r3+1)
+       when Occupation='Actor' then (@r4:=@r4+1) 
+     end as RowNumber,
+     case when Occupation='Doctor' then Name end as Doctor,
+     case when Occupation='Professor' then Name end as Professor,
+     case when Occupation='Singer' then Name end as Singer,
+     case when Occupation='Actor' then Name end as Actor
+  from OCCUPATIONS
+  order by Name
+) Temp
+group by RowNumber
+			     
