f0e70b8b43c7ec8293cb206114911e66e7534b68
Sindhuri Mamidi <sindhuri.mamidi30@gmail.com>
2021-02-09 23:16:39 -0800

    Update Leetcode.sql

--- Leetcode.sql
+++ Leetcode.sql
@@ -2596,3 +2596,56 @@ WITH CTE AS (
 SELECT person1, person2, COUNT(person1) call_count , SUM(duration) total_duration 
 FROM CTE
 GROUP BY person1, person2 
+
+--1285. Find the Start and End Number of Continuous Ranges
+/*Table: Logs
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| log_id        | int     |
++---------------+---------+
+id is the primary key for this table.
+Each row of this table contains the ID in a log Table.
+
+Since some IDs have been removed from Logs. Write an SQL query to find the start and end number of continuous ranges in table Logs.
+
+Order the result table by start_id.
+
+The query result format is in the following example:
+
+Logs table:
++------------+
+| log_id     |
++------------+
+| 1          |
+| 2          |
+| 3          |
+| 7          |
+| 8          |
+| 10         |
++------------+
+
+Result table:
++------------+--------------+
+| start_id   | end_id       |
++------------+--------------+
+| 1          | 3            |
+| 7          | 8            |
+| 10         | 10           |
++------------+--------------+
+The result table should contain all ranges in table Logs.
+From 1 to 3 is contained in the table.
+From 4 to 6 is missing in the table
+From 7 to 8 is contained in the table.
+Number 9 is missing in the table.
+Number 10 is contained in the table.*/
+with cte
+as
+( select log_id, log_id - row_number() over(order by log_id) Grp
+    from Logs
+)
+select min(log_id) start_id, 
+       max(log_id) end_id
+from cte
+group by Grp
